ae9fd60acdbeba292f357e04e78a2459ab0dc866
Guido <alvarezguido87@gmail.com>
2025-02-03 13:13:45 -0300

    Guardar compra y obtener detalle

--- src/main/java/com/eldar/eldar/controller/CompraController.java
+++ src/main/java/com/eldar/eldar/controller/CompraController.java
@@ -1,5 +1,6 @@
 package com.eldar.eldar.controller;
 
+import com.eldar.eldar.model.Compra;
 import com.eldar.eldar.service.CompraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,5 +14,10 @@ public class CompraController {
     @PostMapping
     public void realizarCompra(@RequestParam Double monto, @RequestParam String detalle, @RequestParam String pan, @RequestParam String cvv) throws Exception {
         compraService.realizarCompra(monto, detalle, pan, cvv);
+    }
+
+    @GetMapping("/{id}")
+    public Compra obtenerDetalle(@PathVariable Long id) {
+        return compraService.obtenerDetalle(id);
     }
 }
